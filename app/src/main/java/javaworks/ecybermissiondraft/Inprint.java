bb5e458accc56410f77bbe12575d3ec3774ea18c
flamecaptain <jkempe04@gmail.com>
2018-12-21 09:04:04 -0500

    things

---
+++ app/src/main/java/javaworks/ecybermissiondraft/Inprint.java
@@ -0,0 +1,14 @@
+package javaworks.ecybermissiondraft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inprint extends AppCompatActivity {
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inprint);
+    }
+}
