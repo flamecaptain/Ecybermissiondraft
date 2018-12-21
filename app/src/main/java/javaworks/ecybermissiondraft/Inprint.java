b49bfb898d2aa9d2b2d83f71cfa455257e8d1e45
flamecaptain <jkempe04@gmail.com>
2018-12-21 09:07:44 -0500

    things

--- app/src/main/java/javaworks/ecybermissiondraft/Inprint.java
+++ app/src/main/java/javaworks/ecybermissiondraft/Inprint.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Inprint extends AppCompatActivity {
-    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
